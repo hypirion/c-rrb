8cf0eed692f012f61f6ecc2d96e2e8dea2fc9da6
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-10-11 14:42:56 +0200

    Implement rrb_destroy, rrb_count.

--- src/rrb.c
+++ src/rrb.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdint.h>
+#include <string.h>
 #include "rrb.h"
 
 static rrb_node_t EMPTY_NODE = {.size_table = NULL,
@@ -13,4 +14,12 @@ static inline rrb_node_t *node_create(uint32_t children) {
 
 rrb_t *rrb_create() {
   return NULL;
+}
+
+void rrb_destroy(rrb_t *restrict rrb) {
+  return;
+}
+
+uint32_t rrb_count(const rrb_t *restrict rrb) {
+  return rrb->cnt;
 }
