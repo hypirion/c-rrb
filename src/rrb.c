0cf912899acf8eb4849936557098a09afd146269
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-10-11 14:43:39 +0200

    Implement node_for.

--- src/rrb.c
+++ src/rrb.c
@@ -23,3 +23,30 @@ void rrb_destroy(rrb_t *restrict rrb) {
 uint32_t rrb_count(const rrb_t *restrict rrb) {
   return rrb->cnt;
 }
+
+// Iffy, need to pass back the modified i somehow.
+static rrb_node_t *node_for(const rrb_t *restrict rrb, uint32_t i) {
+  if (i < rrb->cnt) {
+    rrb_node_t *node = rrb->root;
+    for (uint32_t level = rrb->shift; level >= RRB_BITS; level -= RRB_BITS) {
+      uint32_t idx = (i >> level) & RRB_MASK; // TODO: don't think we need the mask
+
+      // vv TODO: I *think* this should be sufficient, not 100% sure.
+      if (node->size_table[idx] <= i) {
+        idx++;
+        if (node->size_table[idx] <= i) {
+          idx++;
+        }
+      }
+
+      if (idx) {
+        i -= node->size_table[idx];
+      }
+      node = node->child[idx];
+    }
+  }
+  // Index out of bounds
+  else {
+    return NULL; // Or some error later on
+  }
+}
