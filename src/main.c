aeaaccc78207d981eae9806ccc8213d610bb17b5
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-10-29 01:58:49 +0100

    Perform more visualization.

--- src/main.c
+++ src/main.c
@@ -27,10 +27,10 @@
 #include "rrb.h"
 
 int main() {
-  int top = 40;
+  uint32_t top = 40;
   const RRB *rrb1 = rrb_create();
   const RRB *rrb2 = rrb_create();
-  for (int i = 0; i < top; i++) {
+  for (uint32_t i = 0; i < top; i++) {
     rrb1 = rrb_push(rrb1, (uintptr_t) i);
     rrb2 = rrb_push(rrb2, (uintptr_t) (i + top));
     char str[80];
@@ -41,6 +41,30 @@ int main() {
     for (uint32_t j = 0; j < rrb_count(catted); j++) {
       printf("%lx ", (uintptr_t) rrb_nth(catted, j));
     }
-    puts("\n");
+    puts("\n\n");
+    if (i > 10) {
+      const RRB* sliced = rrb_slice(catted, 5, (uint32_t) (i - 5));
+      sprintf(str, "img/sliced-%03d.dot", i);
+      rrb_to_dot_file(sliced, str);
+      printf("img/sliced-%03d.dot\n", i);
+      for (uint32_t j = 0; j < rrb_count(sliced); j++) {
+        printf("%lx ", (uintptr_t) rrb_nth(sliced, j));
+      }
+      puts("\n\n");
+    }
+    // testing updates here
+    const RRB* updated = rrb_update(catted, i, (uintptr_t) 0x1337);
+    sprintf(str, "img/updated-%03d.dot", i);
+    DotFile dot = dot_file_create(str);
+    rrb_to_dot(dot, updated);
+    label_pointer(dot, updated, "updated");
+    rrb_to_dot(dot, catted);
+    label_pointer(dot, catted, "catted");
+    dot_file_close(dot);
+    printf("img/updated-%03d.dot\n", i);
+    for (uint32_t j = 0; j < rrb_count(updated); j++) {
+      printf("%lx ", (uintptr_t) rrb_nth(updated, j));
+    }
+    puts("\n\n");
   }
 }
