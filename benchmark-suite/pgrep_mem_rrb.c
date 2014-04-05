c7dd1d95474648726f769fe881a74544f4b8953b
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-04-05 18:30:09 +0200

    Update year in copying.

--- benchmark-suite/pgrep_mem_rrb.c
+++ benchmark-suite/pgrep_mem_rrb.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2013 Jean Niklas L'orange. All rights reserved.
+ * Copyright (c) 2013-2014 Jean Niklas L'orange. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
