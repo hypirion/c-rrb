a43ad153cdf080136d42e62c718ca3aefec2f6a1
Jean Niklas L'orange <jeannikl@hypirion.com>
2014-04-23 17:56:22 +0200

    Implement rrb_to_transient

--- src/rrb_transients.c
+++ src/rrb_transients.c
@@ -41,6 +41,12 @@ struct _TransientRRB {
 static void* rrb_guid_create(void);
 static TransientRRB* transient_rrb_head_create(const RRB* rrb);
 static void check_transience(const TransientRRB *trrb);
+
+static InternalNode* transient_internal_node_create(void);
+static LeafNode* transient_leaf_node_create(void);
+static InternalNode* transient_internal_node_clone(const InternalNode *internal, void *guid);
+static LeafNode* transient_leaf_node_clone(const LeafNode *leaf, void *guid);
+
 static InternalNode* ensure_internal_editable(InternalNode *internal, void *guid);
 static LeafNode* ensure_leaf_editable(LeafNode *leaf, void *guid);
 
@@ -48,12 +54,10 @@ static void* rrb_guid_create() {
   return (void *) RRB_MALLOC_ATOMIC(1);
 }
 
-// creates a new guid -- at least for now. Check if necessary later on.
 static TransientRRB* transient_rrb_head_create(const RRB* rrb) {
   TransientRRB *trrb = RRB_MALLOC(sizeof(TransientRRB));
   memcpy(rrb, trrb, sizeof(RRB));
   trrb->owner = RRB_THREAD_ID();
-  trrb->guid = rrb_guid_create();
   return trrb;
 }
 
@@ -68,14 +72,42 @@ static void check_transience(const TransientRRB *trrb) {
   }
 }
 
+static InternalNode* transient_internal_node_create() {
+  InternalNode *node = RRB_MALLOC(sizeof(InternalNode)
+                              + RRB_BRANCHING * sizeof(InternalNode *));
+  node->type = INTERNAL_NODE;
+  node->size_table = NULL;
+  return node;
+}
+
+static LeafNode* transient_leaf_node_create() {
+  LeafNode *node = RRB_MALLOC(sizeof(LeafNode)
+                                  + RRB_BRANCHING * sizeof(void *));
+  node->type = LEAF_NODE;
+  return node;
+}
+
+static InternalNode* transient_internal_node_clone(const InternalNode *internal, void *guid) {
+  InternalNode *copy = transient_internal_node_create();
+  memcpy(copy, internal,
+         sizeof(InternalNode) + internal->len * sizeof(InternalNode *));
+  copy->guid = guid;
+  return copy;
+}
+
+static LeafNode* transient_leaf_node_clone(const LeafNode *leaf, void *guid) {
+  LeafNode *copy = transient_leaf_node_create();
+  memcpy(copy, leaf, sizeof(LeafNode) + leaf->len * sizeof(void *));
+  copy->guid = guid;
+  return copy;
+}
+
 static InternalNode* ensure_internal_editable(InternalNode *internal, void *guid) {
   if (internal->guid == guid) {
     return internal;
   }
   else {
-    InternalNode *copy = internal_node_clone(internal);
-    copy->guid = guid;
-    return copy;
+    return transient_internal_node_clone(internal, guid);
   }
 }
 
@@ -84,11 +116,25 @@ static LeafNode* ensure_leaf_editable(LeafNode *leaf, void *guid) {
     return leaf;
   }
   else {
-    LeafNode *copy = leaf_node_clone(leaf);
-    copy->guid = guid;
-    return copy;
+    return transient_leaf_node_clone(leaf, guid);
   }
 }
 
-
+TransientRRB* rrb_to_transient(const RRB *rrb) {
+  TransientRRB* trrb = transient_rrb_head_create(rrb);
+  const void *guid = rrb_guid_create();
+  trrb->guid = guid;
+#ifdef RRB_TAIL
+  trrb->tail = transient_leaf_node_clone(rrb->tail, guid);
+#endif
+  if (trrb->root != NULL) {
+    if (trrb->shift == LEAF_NODE_SHIFT) {
+      trrb->root = transient_leaf_node_clone((const LeafNode *)rrb->root, guid);
+    }
+    else {
+      trrb->root = transient_internal_node_clone(rrb->root, guid);
+    }
+  }
+  return trrb;
+}
 #endif
